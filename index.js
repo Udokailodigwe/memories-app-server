b88149ab307bde9f1d66fdf9e4dbe23401e38e01
Udokailodigwe <ilodigweudoka@gmail.com>
2022-06-17 17:12:55 +0300

    Implemented express and other middlewares function to enable routes to endpoints

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+dotenv.config();
 
 const app = express();
-dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 
